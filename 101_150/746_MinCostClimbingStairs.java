978cc954b9b3c51cbf10159abe614f6364f9263d
Dhivakar <dhivakar2108@gmail.com>
2026-05-03 16:28:39 +0530

    dpProblems_Easy

---
+++ 101_150/746_MinCostClimbingStairs.java
@@ -0,0 +1,16 @@
+// Problem 746: Min Cost Climbing Stairs
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n= cost.length;
+        int prev1 = cost[1];
+        int prev2 = cost[0];
+        for(int i=2;i<n;i++){
+            int curr = cost[i] + Math.min(prev1,prev2);
+            prev2 = prev1;
+            prev1 = curr;
+
+        }
+        return Math.min(prev1,prev2);
+    }
+}
